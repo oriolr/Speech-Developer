790864bb41b3a85405e723497af9b9e888500b4d
Rosler <oriolr32@gmail.com>
2020-09-20 00:22:01 -0400

    Stylesheet added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import UserForm from './components/UserForm'
+import './App.css'
 
 export class App extends Component {
     render() {
         return (
             <div className="App">
-               <UserForm /> 
+                <div className="container">
+                 <UserForm /> 
+               </div>
             </div>
         )
     }
